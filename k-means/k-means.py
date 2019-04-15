6003b95969c36640dab43b1ac42ff7228d9ac1c6
Your Name <lucianzhong@gmail.com>
2019-04-15 11:10:56 +0800

    2019-04-15

--- k-means/k-means.py
+++ k-means/k-means.py
@@ -42,13 +42,14 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
     m = shape(dataSet)[0]                   # 行数
     clusterAssment = mat(zeros((m,2)))      #create mat to assign data points  to a centroid, also holds SE of each point
     centroids = createCent(dataSet, k)      # 创建质心，随机k个质心
+    print("centroids",centroids)
     clusterChanged = True 
     while clusterChanged:
         clusterChanged = False
         for i in range(m):                  #循环每一个数据点并分配到最近的质心中去
             minDist = inf;
             minIndex = -1
-            for j in range(k):
+            for j in range(k):              #环每一个质心
                 distJI = distMeas(centroids[j,:],dataSet[i,:])          # 计算数据点到质心的距离
                 if distJI < minDist:                                    # 如果距离比 minDist（最小距离）还小，更新 minDist（最小距离）和最小质心的 index（索引）
                     minDist = distJI;
@@ -73,36 +74,39 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
 	选择使得误差最小的那个簇进行划分
 '''
 
+#该算法首先将所有点作为一个簇，然后将该簇一分为二,之后选择其中一个簇继续进行划分，选择哪一个簇进行划分取决于对其划分时候可以最大程度降低 SSE（平方和误差）的值。
+#上述基于 SSE 的划分过程不断重复，直到得到用户指定的簇数目为止
 # 二分 KMeans 聚类算法, 基于 kMeans 基础之上的优化，以避免陷入局部最小值
 def biKmeans(dataSet, k, distMeas=distEclud):
     m = shape(dataSet)[0]
     clusterAssment = mat(zeros((m,2)))
-    centroid0 = mean(dataSet, axis=0).tolist()[0]   # 质心初始化为所有数据点的均值
-    centList =[centroid0] #create a list with one centroid   # 初始化只有 1 个质心的 list
-    for j in range(m):#calc initial Error   # 计算所有数据点到初始质心的距离平方误差
-        clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2    # 保存每个数据点的簇分配结果和平方误差
+    centroid0 = mean(dataSet, axis=0).tolist()[0]   # 质心初始化为所有数据点的均值  #tolist(),将数组或者矩阵转换成列表 
+    #print("centroid0",centroid0)
+    centList =[centroid0]                           #create a list with one centroid   # 初始化只有 1 个质心的 list
+    for j in range(m):                              #calc initial Error   # 计算所有数据点到初始质心的距离平方误差
+        clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2   # 保存每个数据点的簇分配结果和平方误差
     while (len(centList) < k):  # 当质心数量小于 k 时
         lowestSSE = inf
-        for i in range(len(centList)):   # 对每一个质心
-            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]#get the data points currently in cluster i   # 获取当前簇 i 下的所有数据点
-            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)    # 将当前簇 i 进行二分 kMeans 处理
-            sseSplit = sum(splitClustAss[:,1])#compare the SSE to the currrent minimum   # 将二分 kMeans 结果中的平方和的距离进行求和
+        for i in range(len(centList)):                                             # 对每一个质心
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]     #get the data points currently in cluster i   # 获取当前簇 i 下的所有数据点
+            centroidMat,splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)      # 将当前簇 i 进行k-Means 处理
+            sseSplit = sum(splitClustAss[:,1])                                     #compare the SSE to the currrent minimum   # 将二分 kMeans 结果中的平方和的距离进行求和
             sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])   # 将未参与二分 kMeans 分配结果中的平方和的距离进行求和
-            print ("sseSplit, and notSplit: ",sseSplit,sseNotSplit)
-            if (sseSplit + sseNotSplit) < lowestSSE:   # 总的（未拆分和已拆分）误差和越小，越相似，效果越优化，划分的结果更好（注意：这里的理解很重要，不明白的地方可以和我们一起讨论）
+            #print ("sseSplit, and notSplit: ",sseSplit,sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSE:   # 总的（未拆分和已拆分）误差和越小，越相似，效果越优化，划分的结果更好
                 bestCentToSplit = i
                 bestNewCents = centroidMat
                 bestClustAss = splitClustAss.copy()
                 lowestSSE = sseSplit + sseNotSplit
         # 找出最好的簇分配结果 
-        bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList) #change 1 to 3,4, or whatever   # 调用二分 kMeans 的结果，默认簇是 0,1. 当然也可以改成其它的数字
+        bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList)    #change 1 to 3,4, or whatever   # 调用二分 kMeans 的结果，默认簇是 0,1. 当然也可以改成其它的数字
         bestClustAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit  # 更新为最佳质心
-        print ('the bestCentToSplit is: ',bestCentToSplit )
-        print ('the len of bestClustAss is: ', len(bestClustAss) )
+        #print ('the bestCentToSplit is: ',bestCentToSplit )
+        #print ('the len of bestClustAss is: ', len(bestClustAss) )
         # 更新质心列表
-        centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]#replace a centroid with two best centroids    # 更新原质心 list 中的第 i 个质心为使用二分 kMeans 后 bestNewCents 的第一个质心
-        centList.append(bestNewCents[1,:].tolist()[0])  # 添加 bestNewCents 的第二个质心
-        clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss#reassign new clusters, and SSE  # 重新分配最好簇下的数据（质心）以及SSE
+        centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]   #replace a centroid with two best centroids    # 更新原质心 list 中的第 i 个质心为使用二分 kMeans 后 bestNewCents 的第一个质心
+        centList.append(bestNewCents[1,:].tolist()[0])              # 添加 bestNewCents 的第二个质心
+        clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss   #reassign new clusters, and SSE  # 重新分配最好簇下的数据（质心）以及SSE
     return mat(centList), clusterAssment
 
 
@@ -115,7 +119,7 @@ def biKmeans(dataSet, k, distMeas=distEclud):
 if __name__ == "__main__":
     dataMat=mat( loadDataSet('testSet.txt') )
     #print("dataMat",(dataMat))
-    print ( randCent(dataMat,2) )
+    #print ( randCent(dataMat,2) )
 
     """
     plt.figure()
@@ -129,16 +133,22 @@ if __name__ == "__main__":
     #print ( distEclud(dataMat[0],dataMat[1]) )
     #print("dataMat[0]",dataMat[0])
 
-    myCentroids,clusterAssing=kMeans(dataMat,4)
-    print("myCentroids",myCentroids)
-    print("clusterAssing",clusterAssing)
-
+    #myCentroids,clusterAssing=kMeans(dataMat,4)
+    #print("myCentroids",myCentroids)
+    #print("clusterAssing",clusterAssing)
+    """
     plt.figure()
     plt.plot(dataMat[:,0],dataMat[:,1],'.')
     plt.plot(myCentroids[:,0],myCentroids[:,1],'ro')
     plt.show()
+    """
 
-    #dataMat2=mat( loadDataSet('testSet2.txt') )
-    #biKmeans(dataMat2,3)
+    dataMat2=mat( loadDataSet('testSet2.txt') )
+    myCentroids,clusterAssing=biKmeans(dataMat2,3)
+
+    plt.figure()
+    plt.plot(dataMat2[:,0],dataMat2[:,1],'.')
+    plt.plot(myCentroids[:,0],myCentroids[:,1],'ro')
+    plt.show()
 
 
