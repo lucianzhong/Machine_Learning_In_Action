a6c62f15729d6d17da96ec8d04a070053a5e541e
Your Name <lucianzhong@gmail.com>
2019-05-23 16:42:39 +0800

    2019-05-23

--- Decision_Tree/Decision_Tree.py
+++ Decision_Tree/Decision_Tree.py
@@ -1,4 +1,31 @@
 #-*- coding: utf-8 -*-
+
+
+
+"""
+k-近邻算法可以完成很多分类任务,但是它最大的缺点就是无法给出数据的内在含义,决策树的主要优势就在于数据形式非常容易理解
+决策树的一个重要任务是为了数据中所蕴含的知识信息,因此决策树可以使用不熟悉的数据集合,并从中提取出一决策树的构造系列规则,在这些机器根据数据集创建规则时,就是机器学习的过程。专家系统中经常使用决策
+树,而且决策树给出结果往往可以匹敌在当前领域具有几十年工作经验的人类专家
+
+优点:计算复杂度不高,输出结果易于理解,对中间值的缺失不敏感,可以处理不相关特
+征数据。
+缺点:可能会产生过度匹配问题。 
+适用数据类型:数值型和标称型。
+
+在划分数据集之前之后信息发生的变化称为信息增益,知道如何计算信息增益,我们就可以
+计算每个特征值划分数据集获得的信息增益,获得信息增益最高的特征就是最好的选择。
+在可以评测哪种数据划分方式是最好的数据划分之前,我们必须学习如何计算信息增益。集
+合信息的度量方式称为香农熵或者简称为熵,这个名字来源于信息论之父克劳德·香农。
+
+
+决策树分类器就像带有终止块的流程图,终止块表示分类结果。开始处理数据集时,我们首
+先需要测量集合中数据的不一致性,也就是熵,然后寻找最优方案划分数据集,直到数据集中的
+所有数据属于同一分类。ID3算法可以用于划分标称型数据集。构建决策树时,我们通常采用递
+归的方法将数据集转化为决策树。一般我们并不构造新的数据结构,而是使用Python语言内嵌的
+数据结构字典存储树节点信息。
+
+"""
+
 from numpy import *
 from math import log
 import operator    
@@ -28,8 +55,8 @@ def calcShannonEnt(dataSet):
     numEntries = len(dataSet)                    # 求list的长度，表示计算参与训练的数据量
     #print("numEntries",numEntries)
     labelCounts = {}                             #字典数据结构，键为label
-    for featVec in dataSet: #the the number of unique elements and their occurance  # 将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
-        currentLabel = featVec[-1] # myDat [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    for featVec in dataSet:            #the the number of unique elements and their occurance  # 将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
+        currentLabel = featVec[-1]     # myDat [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
         #print("currentLabel",currentLabel)
         if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1    # 为所有可能的分类创建字典，如果当前的键值不存在，则扩展字典并将当前键值加入字典。每个键值都记录了当前类别出现的次数。
@@ -38,22 +65,23 @@ def calcShannonEnt(dataSet):
     #print("labelCounts",labelCounts)
     for key in labelCounts:                             # 对于 label 标签的占比，求出 label 标签的香农熵
         prob = float(labelCounts[key])/numEntries
-        shannonEnt -= prob * log(prob,2) #log base 2
+        shannonEnt -= prob * log(prob,2) # log base 2, 以2为底求对数
     return shannonEnt
 
 
-# 将指定特征的特征值等于 value 的行剩下列作为子数据集。
-def splitDataSet(dataSet, axis, value):
-    retDataSet = []
+# 将指定特征的特征值等于 value 的行剩下列作为子数据集 / 按照给定特征划分数据集  / 当我们按照某个特征划分数据集时,就需要将所有符合要求的元素抽取出来
+def splitDataSet(dataSet, axis, value):         # 待划分的数据集、划分数据集的特征、需要返回的特征的值
+    retDataSet = []                             # 创建新的list对象
     for featVec in dataSet:
     	#print("featVec",featVec)
         if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
+            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting  将符合特征的数据抽取出来
             reducedFeatVec.extend(featVec[axis+1:])
             retDataSet.append(reducedFeatVec)  # the difference between 'extend' and 'append'
     return retDataSet
 
 
+# 我们将对每个特征划分数据集的结果计算一次信息熵,然后判断按照哪个特征划分数据集是最好的划分方式。
 # choose the best split based on max entropy   选择最好的数据集划分方式
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1                   #the last column is used for the labels   # 求第一行有多少列的 Feature, 最后一列是label列嘛
@@ -61,21 +89,23 @@ def chooseBestFeatureToSplit(dataSet):
     baseEntropy = calcShannonEnt(dataSet)               # 数据集的原始信息熵
     bestInfoGain = 0.0;                                 # 最优的信息增益值
     bestFeature = -1                                    #最优的Featurn编号
-
+    # 第1个 for 循环遍历数据集中的所有特征。使用列表推导(List Comprehension)来创建新的列表,将数据集中所有第i个特征值或者所有可能存在的值写入这个新list中
     for i in range(numFeatures):        #iterate over all the features  2
         featList = [example[i] for example in dataSet]#create a list of all the examples of this feature  # 获取对应的feature下的所有数据
         #print("featList",featList)
-        uniqueVals = set(featList)       #get a set of unique values    # 获取剔重后的集合，使用set对list数据进行去重
+        uniqueVals = set(featList)       #get a set of unique values    # 获取剔重后的集合，使用set对list数据进行去重  创建唯一的分类标签列表
         #print("uniqueVals",uniqueVals)
         newEntropy = 0.0
+        # 遍历当前特征中的所有唯一属性值,对每个特征划分一次数据集
         for value in uniqueVals:                                        # 遍历某一列的value集合，计算该列的信息熵 
+            # 计算每种划分方式的信息熵
             subDataSet = splitDataSet(dataSet, i, value)                # 遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集，计算数据集的新熵值，并对所有唯一特征值得到的熵求和
             #print("value",value)
             #print("subDataSet",subDataSet)
             prob = len(subDataSet)/float(len(dataSet))                  # 计算概率
             newEntropy += prob * calcShannonEnt(subDataSet)             # 计算信息熵  #计算新的香农熵的时候使用的是子集
         infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
-        if (infoGain > bestInfoGain):       #compare this to the best gain so far
+        if (infoGain > bestInfoGain):       # 计算最好的信息增益 compare this to the best gain so far
             bestInfoGain = infoGain         #if better than current best, set to best
             bestFeature = i
     return bestFeature                      #returns an integer
@@ -92,12 +122,16 @@ def majorityCnt(classList):
 # 如果数据集的最后一列的第一个值出现的次数=整个集合的数量，也就说只有一个类别，就只直接返回结果就行
 # 第一个停止条件：所有的类标签完全相同，则直接返回该类标签。
 # count() 函数是统计括号中的值在list中出现的次数
-def createTree(dataSet,labels):
+# 递归结束的条件是:程序遍历完所有划分数据集的属性,或者每个分支下的所有实例都具有相同的分类。如果所有实例具有相同的分类,则得到一个叶子节点或者终止块。任何到达叶子节点的数据必然属于叶子节点的分类,
+
+def createTree(dataSet,labels):                         # 数据集和标签列表
     classList = [example[-1] for example in dataSet]
     # 如果数据集只有1列，那么最初出现label次数最多的一类，作为结果
+    # 类别完全相同则停止继续划分
     if classList.count(classList[0]) == len(classList): 
         return classList[0]#stop splitting when all of the classes are equal
     # 第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组。
+    # 遍历完所有特征时返回出现次数最多的
     if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
         return majorityCnt(classList)
 
@@ -108,7 +142,7 @@ def createTree(dataSet,labels):
     # 注：labels列表是可变对象，在PYTHON函数中作为参数时传址引用，能够被全局修改
     # 所以这行代码导致函数外的同名变量被删除了元素，造成例句无法执行，提示'no surfacing' is not in list
     del(labels[bestFeat])
-    featValues = [example[bestFeat] for example in dataSet]
+    featValues = [example[bestFeat] for example in dataSet]  # 得到列表包含的所有属性值
     uniqueVals = set(featValues)
     for value in uniqueVals:
         subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
@@ -152,7 +186,7 @@ def plotTree(tree):
 def classify(inputTree, featLabels, testVec):
     firstStr = list(inputTree.keys())[0]
     secondDict = inputTree[firstStr]
-    featIndex = featLabels.index(firstStr)
+    featIndex = featLabels.index(firstStr)   # 将标签字符串转换为索引
     for key in secondDict.keys():
         if testVec[featIndex] == key:
             if type(secondDict[key]).__name__ == 'dict':
@@ -174,6 +208,7 @@ if __name__ == "__main__":
     #print ("splitDataSet(myDat, 0, 0)",splitDataSet(myDat, 0, 0))
 
 
+    #熵越高,则混合的数据也越多,我们可以在数据集中添加更多的分类,观察熵是如何变化的
     #myDat[0][-1]='maybe'
     #print ( calcShannonEnt(myDat) )  #1.37095059445  熵越高，混合的数据越多
     #print ( splitDataSet(myDat,0,1))
@@ -186,6 +221,7 @@ if __name__ == "__main__":
     plotTree(myTree)
     dot.view()
 
+################################################################################
     # 使用决策树预测隐形眼睛类型
     with open('lenses.txt') as f:
         lenses = [item.strip().split('\t') for item in f.readlines()]
--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,11 +1,41 @@
 #-*- coding: utf-8 -*-
+
+# KNN
+"""
+那么动作片具有哪些共有特征,使得动作片之间非常类似,
+而与爱情片存在着明显的差别呢?动作片中也会存在接吻镜头,爱情片中也会存在打斗场景,我们
+不能单纯依靠是否存在打斗或者亲吻来判断影片的类型。但是爱情片中的亲吻镜头更多,动作片中
+的打斗场景也更频繁,基于此类场景在某部电影中出现的次数可以用来进行电影分类。本章第一节
+基于电影中出现的亲吻、打斗出现的次数,使用k-近邻算法构造程序,自动划分电影的题材类型。
+
+简单地说,k-近邻算法采用测量不同特征值之间的距离方法进行分类。
+k-近邻算法
+优点:精度高、对异常值不敏感、无数据输入假定。
+缺点:计算复杂度高、空间复杂度高。
+适用数据范围:数值型和标称型
+
+它的工作原理是:存在一个样本数
+据集合,也称作训练样本集,并且样本集中每个数据都存在标签,即我们知道样本集中每一数据
+与所属分类的对应关系。输入没有标签的新数据后,将新数据的每个特征与样本集中数据对应的
+特征进行比较,然后算法提取样本集中特征最相似数据(最近邻)的分类标签。一般来说,我们
+只选择样本数据集中前k个最相似的数据,这就是k-近邻算法中k的出处,通常k是不大于20的整数。
+最后,选择k个最相似数据中出现次数最多的分类,作为新数据的分类。
+
+
+k-近邻算法是分类数据最简单最有效的算法,本章通过两个例子讲述了如何使用k-近邻算法
+构造分类器。k-近邻算法是基于实例的学习,使用算法时我们必须有接近实际数据的训练样本数
+据。k-近邻算法必须保存全部数据集,如果训练数据集的很大,必须使用大量的存储空间。此外,
+由于必须对数据集中的每个数据计算距离值,实际使用时可能非常耗时
+
+"""
+
 from numpy import *
-import operator    
+import operator             # 运算符模块
 from os import listdir
 import matplotlib
 import matplotlib.pyplot as plt
 
-
+##################################################################################################
 def createDataSet():
     group = array(  [ [1.0,1.1],[1.0,1.0],[0,0],[0,0.1] ]  )
     labels = ['A','A','B','B']
@@ -22,51 +52,58 @@ def createDataSet():
 #the basic KNN algorithm
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]  # (4*2 the shape of dataSet)
-    #print("dataSetSize",dataSet.shape)
+    print("dataSetSize",dataSet.shape)
     diffMat = tile(inX, (dataSetSize,1)) - dataSet  #numpy.tile([0,0],(2,1))#在列方向上重复[0,0]1次，行2次,array([[0, 0],[0, 0]])
     #print("diffMat",diffMat)
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1) #axis=1以后就是将一个矩阵的每一行向量相加
-    distances = sqDistances**0.5
+    distances = sqDistances**0.5   # 距离计算  欧氏距离公式
     #print("distances",distances)
     sortedDistIndicies = distances.argsort() #argsort函数返回的是数组值从小到大的索引值
     #print("sortedDistIndicies",sortedDistIndicies)
     classCount={}   #dirctionary
-    # get the nearest K
+    # get the nearest K  选择距离最小的k个点
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1  #get('e', 0) tells python to look for the key 'e' in the dictionary. If it's not found it returns 0
         #print("classCount",classCount)
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    #print("sortedClassCount",sortedClassCount)
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)  # 按照第二个元素的次序对元组进行排序,此处的排序为逆序,即按照从最大到最小次序排序,最后返回发生频率最高的元素标签
+    #print("sortedClassCount",sortedClassCount) 
     return sortedClassCount[0][0]
 
 
+############################################################################################
+# 示例:使用 k-近邻算法改进约会网站的配对效果
+
 # txt file to array
 def file2matrix(filename):
-    fr = open(filename)
+    fr = open(filename)                         # 得到文件行数
     numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return 
+    # 创建返回的NumPy矩阵  然后创建以零填充的矩阵NumPy 为了简化处理,我们将该矩阵的另一维度设置为固定值 3 ,你可以按照自己的实际需求增加相应的代码以适应变化的输入值。循环处理文件中的每行数据
     classLabelVector = []                       #prepare labels return   
     fr = open(filename)
     index = 0
-    for line in fr.readlines(): #readlines() 方法用于读取所有行(直到结束符 EOF)并返回列表
-        line = line.strip()     #Python strip() 方法用于移除字符串头尾指定的字符（默认为空格或换行符）或字符序列
-        listFromLine = line.split('\t')  #Python split() 通过指定分隔符对字符串进行切片
-        returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+    # 解析文件数据到列表
+    for line in fr.readlines():                     # readlines() 方法用于读取所有行(直到结束符 EOF)并返回列表
+        line = line.strip()                         # Python strip() 方法用于移除字符串头尾指定的字符（默认为空格或换行符）或字符序列
+        listFromLine = line.split('\t')             # Python split() 通过指定分隔符对字符串进行切片
+        returnMat[index,:] = listFromLine[0:3]              # 选取前3个元素,将它们存储到特征矩阵中
+        classLabelVector.append(int(listFromLine[-1]))      # 将列表的最后一列存储到向量 classLabelVector 中
         index += 1
     return returnMat,classLabelVector
 
 #normalization  归一化数据
+# 在处理这种不同取值范围的特征值时,我们通常采用的方法是将数值归一化,如将取值范围处理为0到1或者1到1之间。
 def autoNorm(dataSet):
+    # 为了归一化特征值,我们必须使用当前值减去最小值,然后除以取值范围。
     minVals = dataSet.min(0) #axis=0; 每列的最小值
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    normDataSet = dataSet - tile(minVals, (m,1)) #特征值矩阵有1000×3个值,而 minVals 和 range 的值都为1×3。为了解决这个问题,我们使用NumPy库中 tile() 函数将变量内容复制成输入矩阵同样大小的矩阵
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide 特征值相除
     return normDataSet, ranges, minVals
 
 #####################################################################################################
@@ -74,6 +111,7 @@ def autoNorm(dataSet):
 #the date test
 def datingClassTest():
     hoRatio = 0.10      #hold out 50%
+    # 使用了 file2matrix 和 autoNorm() 函数从文件中读取数据并将其转换为归一化特征值
     datingDataMat,datingLabels = file2matrix('datingTestSet.txt')       #load data setfrom file
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
@@ -81,17 +119,17 @@ def datingClassTest():
     errorCount = 0.0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        print ( "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]) )
         if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
-    print errorCount
+    print ("the total error rate is: %f" % (errorCount/float(numTestVecs)) )
+    print (errorCount)
 
 
 
 #the classify a person based on inputs
 def classifyPerson():
     resultList=['not at all','in small doses','in large doses']
-    percentTats=float(raw_input("percentage of time playing video game?"))
+    percentTats=float(raw_input("percentage of time playing video game?")) 
     ffile=float(raw_input("frequent flier miles earned per year ?"))
     icecream=float(raw_input("ice cream consumed per year ?"))
     datingDataMat,datingLabels = file2matrix('datingTestSet.txt')       #load data setfrom file
@@ -103,7 +141,10 @@ def classifyPerson():
 
 
 #####################################################################################################
-#kNN算法_手写识别实例
+# kNN算法_手写识别实例
+# 需要识别的数字已经使用图形处理软件,处理成具有相同的色彩和大小 1 :宽高是32像素×32像素的黑白图像。
+
+# 将图像格式化处理为一个向量。我们将把一个32×32的二进制图像矩阵转换为1×1024的向量,
 def img2vector(filename):
     returnVect = zeros((1,1024)) #vector(1*1024)
     fr = open(filename)
@@ -117,13 +158,13 @@ def img2vector(filename):
 
 def handwritingClassTest():
     hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
+    trainingFileList = listdir('trainingDigits')           #load the training set 获取目录内容
     m = len(trainingFileList)
     trainingMat = zeros((m,1024))
     for i in range(m):
-        fileNameStr = trainingFileList[i]
+        fileNameStr = trainingFileList[i]       # 从文件名解析分类数字
         fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
+        classNumStr = int(fileStr.split('_')[0]) # 该目录下的文件按照规则命名,如文件9_45.txt的分类是9,它是数字9的第45个实例
         hwLabels.append(classNumStr)
         trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
     testFileList = listdir('testDigits')        #iterate through the test set
@@ -135,11 +176,10 @@ def handwritingClassTest():
         classNumStr = int(fileStr.split('_')[0])
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr) )
         if (classifierResult != classNumStr): errorCount += 1.0
-    print "\nthe total number of errors is: %d" % errorCount
-    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
-
+    print ("\nthe total number of errors is: %d" % errorCount )
+    print ("\nthe total error rate is: %f" % (errorCount/float(mTest)) )
 
 
 
@@ -164,6 +204,7 @@ if __name__ == "__main__":
     ax=fig.add_subplot(111)  #111”表示“1×1网格，第一子图”，“234”表示“2×3网格，第四子图”。
     ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15*array(datingLabels),15*array(datingLabels)) # scatter
     plt.show()
+    # 散点图使用datingDataMat矩阵的第二、第三列数据,分别表示特征值“玩视频游戏所耗时间百分比”和“每周所消费的冰淇淋公升数”
 
    
 
